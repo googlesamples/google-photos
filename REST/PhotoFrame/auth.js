3fbab5c808f8cdc40c31ac251256e15bd4286ecc
Jan-Felix Schmakeit <jfschmakeit@google.com>
2019-01-11 17:01:38 +1100

    Upgrade packages and remove G+ API dependency
    Change-Id: Iea3f4b9f18a09b60fb1269cf7e526eb9817e3fef

--- REST/PhotoFrame/auth.js
+++ REST/PhotoFrame/auth.js
@@ -14,15 +14,17 @@
 
 const config = require('./config.js');
 
-const GoogleOAuth2Strategy = require('passport-google-oauth').OAuth2Strategy;
+const GoogleOAuthStrategy = require('passport-google-oauth20').Strategy;
 module.exports = (passport) => {
   passport.serializeUser((user, done) => done(null, user));
   passport.deserializeUser((user, done) => done(null, user));
-  passport.use(new GoogleOAuth2Strategy(
+  passport.use(new GoogleOAuthStrategy(
       {
         clientID: config.oAuthClientID,
         clientSecret: config.oAuthclientSecret,
         callbackURL: config.oAuthCallbackUrl,
+        // Set the correct profile URL that does not require any additional APIs
+        userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
       },
       (token, refreshToken, profile, done) => done(null, {profile, token})));
 };
